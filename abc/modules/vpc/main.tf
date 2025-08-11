b259c4bff4250acf43385702391c8b0eb1f01b6e
Aslammohammad11 <aslammohammad19913@gmail.com>
2025-08-11 13:49:04 +0530

    added ingress in VPC

--- abc/modules/vpc/main.tf
+++ abc/modules/vpc/main.tf
@@ -58,6 +58,18 @@ resource "aws_security_group" "mysecuritygroup" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
+  ingress {
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    }   
 
   egress {
     from_port   = 0
